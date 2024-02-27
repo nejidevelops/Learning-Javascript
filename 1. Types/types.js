435e83200d01b6edd3589bf91c46dd32b860fbf4
Newton Bofulo <newtonombese1@gmail.com>
2024-02-27 08:55:02 +0300

    Working on the complex data types

--- 1. Types/types.js
+++ 1. Types/types.js
@@ -23,3 +23,31 @@ console.log(foo, bar);
 /*  @N/B
 Symbols and BigInts cannot be faithfully polyfilled, so they should not be used when targeting browsers/environments that don’t support them natively.
 */
+
+/*
+1.2 Complex: When you access a complex type you work on a reference to its value
+  1. Object
+  2. Array
+  3. Function
+*/
+
+// Array
+const foz  = [1, 2]
+const baz = foz;
+baz.push(3);
+console.log(foz, baz);
+// Output: [1, 2, 3] [1, 2, 3]
+
+// Object
+const foobar = { foo: 1, bar: 2 };
+const barfoo = foobar;
+barfoo.bar = 3;
+console.log(foobar, barfoo);
+// Output: { foo: 1, bar: 3 } { foo: 1, bar: 3 }
+
+// Function
+const barz = (a, b) => a + b;
+const qux = barz;
+qux(2, 3);
+console.log(barz(2, 3));
+// Output: 5

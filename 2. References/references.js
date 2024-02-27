39b1fea9df434fe73c608de167d29e9160d14558
Newton Bofulo <newtonombese1@gmail.com>
2024-02-27 09:32:17 +0300

    Working on the references work

--- 2. References/references.js
+++ 2. References/references.js
@@ -11,7 +11,7 @@ https://eslint.org/docs/rules/no-const-assign
 
 const a = 1;
 const b = a;
-b = 2;
+// b = 2; // This will result to a TypeError: Assignment to constant Variable.
 console.log(a, b);
 // Output: 1 2
 
@@ -19,4 +19,30 @@ console.log(a, b);
 2.2 If you must reassign references, use let instead of var.
 eslint no-var: "error",
 https://eslint.org/docs/latest/rules/no-var
+*/
+
+let count = 1;
+count = 2;
+if (true && count) {
+  count = 3;
+}
+console.log(count);
+// Output: 2
+
+/*
+2.3 Both let and const are block-scoped, whereas var is function-scoped.
+*/
+
+{
+  let m = 1;
+  const n = 2;
+  var o = 3;
+}
+
+console.log(m, n, o);
+// Output: 1 2 3
+/* You'll get this:
+  ReferenceError: m is not Defined
+  ReferenceError: n is not Defined
+  ReferenceError: o will print 3
 */

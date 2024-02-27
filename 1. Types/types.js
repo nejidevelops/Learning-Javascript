ca3fa100276fb5c55e919c79e442fa82720745c6
Newton Bofulo <newtonombese1@gmail.com>
2024-02-27 08:45:34 +0300

    Learning Javascript Types

---
+++ 1. Types/types.js
@@ -0,0 +1,14 @@
+// Types
+/*
+1.1 Primitives - When you access a primitive type you work directly on its value.
+  1. String
+  2. Number
+  3. Boolean
+  4. Null
+  5. Undefined
+  6. Symbol
+  7. BigInt
+  8. Object
+  9. Function
+  10. Array
+*/

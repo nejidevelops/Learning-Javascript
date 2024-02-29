8d5542e516eaa1555dade8e80443e9a4aa6f0a68
Newton Bofulo <newtonombese1@gmail.com>
2024-02-29 18:17:22 +0300

    Setting up a Javascript challenge solving directory

---
+++ 4. Arrays/arrays.js
@@ -0,0 +1,8 @@
+// Arrays
+// Creating an array
+const fruits = ['apple', 'banana', 'orange'];
+
+// Accessing elements
+console.log(fruits[0]); // Output: 'apple'
+console.log(fruits[1]); // Output: 'banana'
+console.log(fruits[2]); // Output: 'orange'
---
+++ Javascript-Practice/README.md
@@ -0,0 +1,1 @@
+### W3 Resource Assignments

7710558ea72676f24d74dcfc71d53b3d0ddcd2ad
Newton Bofulo <newtonombese1@gmail.com>
2024-04-04 07:36:52 +0300

    Setting up the reverse string solution

---
+++ Javascript-Practice/reverse_string.js
@@ -0,0 +1,8 @@
+// Write a function that takes a string as input and returns the string reversed.
+
+function reverseString(str) {
+  return str.split('').reverse('').join('')
+}
+
+reverseString('hello')
+
